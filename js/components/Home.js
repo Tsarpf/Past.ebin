c0e25dc95f1aab81643726335625108d75f60229
Teemu Sarapisto <tsarpf@gmail.com>
2015-07-27 04:35:42 +0300

    Add textarea

--- js/components/Home.js
+++ js/components/Home.js
@@ -4,7 +4,10 @@ import styles from '../../css/app.css';
 export default React.createClass( {
 	render() {
 		return (
-			<h1 className={styles.text}>Start your app here! test</h1>
+			<div>
+				<h1 className={styles.text}>Start your app here! test</h1>
+				<textarea placeholder="paste your stuff here"/>
+			</div>
 		);
 	}
 } );
