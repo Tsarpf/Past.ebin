1ea2a86dd9f5cceb728a94beae90f6d26a318ad5
Teemu Sarapisto <tsarpf@gmail.com>
2015-07-27 04:30:21 +0300

    Change to redux

---
+++ js/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../css/app.css';
+
+export default React.createClass( {
+	render() {
+		return (
+			<h1 className={styles.text}>Start your app here! test</h1>
+		);
+	}
+} );
